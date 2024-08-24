e946fe8515fa5401811b8c7c3fdbad995387f6da
BenasB <29711974+BenasB@users.noreply.github.com>
2024-08-24 23:21:16 +0300

    Formatting and acknowledged schema changes

--- src/api/Compooler.Persistence/Migrations/20240824200346_Initial.cs
+++ src/api/Compooler.Persistence/Migrations/20240824200346_Initial.cs
@@ -16,29 +16,39 @@ namespace Compooler.Persistence.Migrations
                 name: "Rides",
                 columns: table => new
                 {
-                    Id = table.Column<int>(type: "integer", nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Id = table
+                        .Column<int>(type: "integer", nullable: false)
+                        .Annotation(
+                            "Npgsql:ValueGenerationStrategy",
+                            NpgsqlValueGenerationStrategy.IdentityByDefaultColumn
+                        ),
                     DriverId = table.Column<int>(type: "integer", nullable: false),
                     MaxPassengers = table.Column<int>(type: "integer", nullable: false)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Rides", x => x.Id);
-                });
+                }
+            );
 
             migrationBuilder.CreateTable(
                 name: "Users",
                 columns: table => new
                 {
-                    Id = table.Column<int>(type: "integer", nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Id = table
+                        .Column<int>(type: "integer", nullable: false)
+                        .Annotation(
+                            "Npgsql:ValueGenerationStrategy",
+                            NpgsqlValueGenerationStrategy.IdentityByDefaultColumn
+                        ),
                     FirstName = table.Column<string>(type: "text", nullable: false),
                     LastName = table.Column<string>(type: "text", nullable: false)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Users", x => x.Id);
-                });
+                }
+            );
 
             migrationBuilder.CreateTable(
                 name: "RidesPassengers",
@@ -46,7 +56,11 @@ namespace Compooler.Persistence.Migrations
                 {
                     UserId = table.Column<int>(type: "integer", nullable: false),
                     RideId = table.Column<int>(type: "integer", nullable: false),
-                    JoinedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
+                    JoinedAt = table.Column<DateTimeOffset>(
+                        type: "timestamp with time zone",
+                        nullable: false,
+                        defaultValueSql: "CURRENT_TIMESTAMP"
+                    )
                 },
                 constraints: table =>
                 {
@@ -56,18 +70,32 @@ namespace Compooler.Persistence.Migrations
                         column: x => x.RideId,
                         principalTable: "Rides",
                         principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+                        onDelete: ReferentialAction.Cascade
+                    );
+                }
+            );
 
             migrationBuilder.CreateTable(
                 name: "Routes",
                 columns: table => new
                 {
                     RideId = table.Column<int>(type: "integer", nullable: false),
-                    Start_Latitude = table.Column<double>(type: "double precision", nullable: false),
-                    Start_Longitude = table.Column<double>(type: "double precision", nullable: false),
-                    Finish_Latitude = table.Column<double>(type: "double precision", nullable: false),
-                    Finish_Longitude = table.Column<double>(type: "double precision", nullable: false)
+                    Start_Latitude = table.Column<double>(
+                        type: "double precision",
+                        nullable: false
+                    ),
+                    Start_Longitude = table.Column<double>(
+                        type: "double precision",
+                        nullable: false
+                    ),
+                    Finish_Latitude = table.Column<double>(
+                        type: "double precision",
+                        nullable: false
+                    ),
+                    Finish_Longitude = table.Column<double>(
+                        type: "double precision",
+                        nullable: false
+                    )
                 },
                 constraints: table =>
                 {
@@ -77,24 +105,22 @@ namespace Compooler.Persistence.Migrations
                         column: x => x.RideId,
                         principalTable: "Rides",
                         principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+                        onDelete: ReferentialAction.Cascade
+                    );
+                }
+            );
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "RidesPassengers");
+            migrationBuilder.DropTable(name: "RidesPassengers");
 
-            migrationBuilder.DropTable(
-                name: "Routes");
+            migrationBuilder.DropTable(name: "Routes");
 
-            migrationBuilder.DropTable(
-                name: "Users");
+            migrationBuilder.DropTable(name: "Users");
 
-            migrationBuilder.DropTable(
-                name: "Rides");
+            migrationBuilder.DropTable(name: "Rides");
         }
     }
 }
